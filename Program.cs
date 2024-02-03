7f5ad94c3554fb1073cb8478faac073847d19105
narikang <96326428+narikang@users.noreply.github.com>
2024-02-02 23:32:19 -0700

    Final TicTacToe
    This is our final TicTacToe

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    public class Driver() {
+        public static void Main()
+        {
+            Console.WriteLine("Welcome to Tic Tac Toe!");
+            // Create a game board array to store the players' choices
+            char[,] board = {
+                            {' ', ' ', ' '},
+                            {' ', ' ', ' '},
+                            {' ', ' ', ' '}
+                        };
+            // Ask each player in turn for their choice and update the game board array
+            bool player1 = true;
+            do
+            {
+                //DisplayBoard(board);
+                if (player1)
+                {
+                    Console.WriteLine("Player X, where would you like to play?");
+                }
+                else
+                {
+                    Console.WriteLine("Player O, where would you like to play?");
+                }
+                Console.WriteLine("Choose the row then the column. For example, '1,2' would select the Top Middle.");
+                string input = Console.ReadLine();
+                string[] inputParts = input.Split(',');
+                int row = int.Parse(inputParts[0]);
+                int col = int.Parse(inputParts[1]);
+                if (/*ValidateInput(row, col) && */board[row, col] == ' ')
+                {
+                    board[row, col] = player1 ? 'X' : 'O';
+                    player1 = !player1;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input or cell already taken. Try again.");
+                }
+                Supporting.DisplayBoard(board);
+            } while (!Supporting.IsBoardFull(board) && !Supporting.Win(board, !player1));
+        }
+    }
+}
---
+++ Supporting.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    public class Supporting
+    {
+        public static bool Win(char[,] board, bool player1)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                if ((board[i, 0] == board[i, 1] && board[i, 1] == board[i, 2] && board[i, 0] != ' ') || // Check rows
+                            (board[0, i] == board[1, i] && board[1, i] == board[2, i] && board[0, i] != ' ') || // Check columns
+                            (board[0, 0] == board[1, 1] && board[1, 1] == board[2, 2] && board[0, 0] != ' ') || // Check main diagonal
+                            (board[0, 2] == board[1, 1] && board[1, 1] == board[2, 0] && board[0, 2] != ' '))
+                {
+                    if (player1)
+                    {
+                        Console.WriteLine("player1 is the winner");
+                    } else
+                    {
+                        Console.WriteLine("player2 is the winner");
+                    }
+                    
+                    return true;
+                }
+            }
+            
+            return false;
+        }
+
+        public static void DisplayBoard(char[,] board)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write('|');
+
+                for (int c = 0; c < 3; c++)
+                {
+                    Console.Write(board[i, c].ToString() + '|');
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static bool IsBoardFull(char[,] board)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int c = 0; c < 3; c++)
+                {
+                    if (board[i,c] == ' ')
+                    {
+                        return false;
+                    }
+                }
+                
+            }
+            Console.WriteLine("its a tie");
+            return true;
+        }
+    }
+}
+
+
